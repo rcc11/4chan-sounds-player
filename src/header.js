57022665acaa6294a0bdb9029ae98fd52088147b
rcc11 <64926786+rcc11@users.noreply.github.com>
2020-05-09 23:04:57 +0100

    1.0.0

--- src/header.js
+++ src/header.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         4chan sounds player
-// @version      0.3.1
+// @version      1.0.0
 // @namespace    rccom
 // @description  Play that faggy music weeb boi
 // @author       RCC
