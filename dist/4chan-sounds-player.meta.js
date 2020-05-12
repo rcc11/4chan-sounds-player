820ba72da17042cda1b6e109efc82c762d107d37
rcc11 <64926786+rcc11@users.noreply.github.com>
2020-05-12 19:47:29 +0100

    add build

---
+++ dist/4chan-sounds-player.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         4chan sounds player
+// @version      1.2.0
+// @namespace    rccom
+// @description  Play that faggy music weeb boi
+// @author       RCC
+// @match        *://boards.4chan.org/*
+// @match        *://boards.4channel.org/*
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @run-at       document-start
+// @updateURL    https://greasyfork.org/scripts/402682-4chan-sounds/code/4chan%20sounds.user.js
+// @downloadURL  https://greasyfork.org/scripts/402682-4chan-sounds/code/4chan%20sounds.user.js
+// ==/UserScript==
