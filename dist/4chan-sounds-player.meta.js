c97bdae0eec6f1d26af00615315d973e5207073f
rcc11 <64926786+rcc11@users.noreply.github.com>
2020-05-16 22:17:47 +0100

    1.3.1

--- dist/4chan-sounds-player.meta.js
+++ dist/4chan-sounds-player.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         4chan sounds player
-// @version      1.3.0
+// @version      1.3.1
 // @namespace    rccom
 // @description  Play that faggy music weeb boi
 // @author       RCC
