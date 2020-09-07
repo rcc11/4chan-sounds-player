d7bf21ed855c95ff3251c5df4953915fdcd0d4e4
rcc11 <64926786+rcc11@users.noreply.github.com>
2020-09-08 00:45:17 +0100

    beta

--- dist/4chan-sounds-player-beta.meta.js
+++ dist/4chan-sounds-player-beta.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         4chan sounds player
-// @version      3.1.1-78a5096
+// @version      3.1.1-77f3c52
 // @namespace    rccom
 // @description  A player designed for 4chan sounds threads.
 // @author       RCC
