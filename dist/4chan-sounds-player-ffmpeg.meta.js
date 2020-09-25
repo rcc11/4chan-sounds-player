d806d6aa56a6febaae4745a7d84107a1c2111ffd
rcc11 <64926786+rcc11@users.noreply.github.com>
2020-09-25 23:35:59 +0100

    we 3.1.2

--- dist/4chan-sounds-player-ffmpeg.meta.js
+++ dist/4chan-sounds-player-ffmpeg.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         4chan sounds player
-// @version      3.1.1
+// @version      3.1.2
 // @namespace    rccom
 // @description  A player designed for 4chan sounds threads.
 // @author       RCC
