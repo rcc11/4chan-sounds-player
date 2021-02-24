1f45b9b5ede1477facc19f6cda41ddb7a8b2bdd8
rcc11 <64926786+rcc11@users.noreply.github.com>
2021-02-24 23:07:19 +0000

    3.3.2 beta

--- dist/4chan-sounds-player-beta.meta.js
+++ dist/4chan-sounds-player-beta.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         4chan sounds player
-// @version      3.3.1-684b21e
+// @version      3.3.2-77268c1
 // @namespace    rccom
 // @description  A player designed for 4chan sounds threads.
 // @author       RCC
@@ -31,7 +31,7 @@
 // @connect      lewd.se
 // @connect      *
 // @run-at       document-start
-// @require      https://github.com/richtr/NoSleep.js/raw/master/dist/NoSleep.min.js
+// @require      https://gitcdn.xyz/repo/richtr/NoSleep.js/07fcee254724ab1b79076fbc22f3dd447649a2eb/dist/NoSleep.min.js
 // @updateURL    https://raw.githubusercontent.com/rcc11/4chan-sounds-player/master/dist/4chan-sounds-player-beta.meta.js
 // @downloadURL  https://raw.githubusercontent.com/rcc11/4chan-sounds-player/master/dist/4chan-sounds-player-beta.user.js
 // ==/UserScript==
